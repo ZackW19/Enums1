ac179f3efe1e738405db45bf2293970d879bc1ea
ZW <zwajdziak@gmail.com>
2017-08-18 20:37:53 +0200

    x

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         System.out.println();
         System.out.println(person.getName() + " ma makijaż? " + person.getGender().isHasMakeup());
         System.out.println(person1.getName() + " ma makijaż? " + person1.getGender().isHasMakeup());
-        System.out.println("---------------------------------");
+        System.out.println("-----------------------------------------------");
         System.out.println("Opis: " + Weather.CLOUDYDAY.getLongDesc());
         System.out.println("Pada? " + Weather.CLOUDYDAY.isRaining());
         System.out.println("Swieci słońce? " + Weather.CLOUDYDAY.isSunshining());
